e5272e81276a7aceeee1fdde50694e9720bac8ec
Szymon Welgus <szymonwelgus@gmail.com>
2023-04-08 19:26:18 +0200

    feat: create clients migration script and add to resources

---
+++ res/migrations/20230408192035_create_clients_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE clients
+(
+    client_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NULL,
+    date_created INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    date_modified INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    is_active INTEGER NOT NULL DEFAULT (1),
+
+    employer_id INTEGER NOT NULL,
+
+    FOREIGN KEY (employer_id) REFERENCES employers(employer_id)
+);
