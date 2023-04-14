68479b44220d07f10611601ab770a1d3e9f92b2e
Szymon Welgus <szymonwelgus@gmail.com>
2023-04-14 23:58:10 +0200

    feat: add migration script to resources

---
+++ res/migrations/20230414235012_create_projects_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS projects
+(
+    project_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL UNIQUE,
+    display_name TEXT NOT NULL,
+    description TEXT NULL,
+    is_default INTEGER NOT NULL DEFAULT (0),
+    date_created INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    date_modified INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    is_active INTEGER NOT NULL DEFAULT (1),
+
+    employer_id INTEGER NOT NULL,
+    client_id INTEGER NULL,
+
+    FOREIGN KEY (employer_id) REFERENCES employers(employer_id),
+    FOREIGN KEY (client_id) REFERENCES clients(client_id)
+);
