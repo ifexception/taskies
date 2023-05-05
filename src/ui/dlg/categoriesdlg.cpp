f564378058ef2976fb4467dacb53f80df69cf8ed
Szymon Welgus <szymonwelgus@gmail.com>
2023-05-05 20:54:31 +0200

    feat: initial commit of categories dialog

---
+++ src/ui/dlg/categoriesdlg.cpp
@@ -0,0 +1,77 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2023 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#include "categoriesdlg.h"
+
+#include <wx/richtooltip.h>
+#include <wx/statline.h>
+#include <fmt/format.h>
+
+#include "../../common/constants.h"
+#include "../../common/common.h"
+#include "../../core/environment.h"
+#include "../../utils/utils.h"
+
+#include "errordlg.h"
+
+namespace tks::UI::dlg
+{
+CategoriesDialog::CategoriesDialog(wxWindow* parent,
+    std::shared_ptr<Core::Environment> env,
+    std::shared_ptr<spdlog::logger> logger,
+    bool isEdit,
+    const wxString& name)
+{
+}
+
+void CategoriesDialog::Create() {}
+
+void CategoriesDialog::CreateControls() {}
+
+void CategoriesDialog::FillControls() {}
+
+void CategoriesDialog::ConfigureEventBindings() {}
+
+void CategoriesDialog::OnClear(wxCommandEvent& event) {}
+
+void CategoriesDialog::OnAdd(wxCommandEvent& event) {}
+
+void CategoriesDialog::OnEdit(wxCommandEvent& event) {}
+
+void CategoriesDialog::OnRemove(wxCommandEvent& event) {}
+
+void CategoriesDialog::OnRemoveAll(wxCommandEvent& event) {}
+
+void CategoriesDialog::OnOK(wxCommandEvent& event) {}
+
+void CategoriesDialog::OnCancel(wxCommandEvent& event) {}
+
+void CategoriesDialog::OnItemChecked(wxListEvent& event) {}
+
+void CategoriesDialog::OnItemUnchecked(wxListEvent& event) {}
+
+void CategoriesDialog::OnItemRightClick(wxListEvent& event) {}
+
+void CategoriesDialog::ResetControlValues() {}
+
+bool CategoriesDialog::TransferDataAndValidate()
+{
+    return false;
+}
+} // namespace tks::UI::dlg
---
+++ src/ui/dlg/categoriesdlg.h
@@ -0,0 +1,83 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2023 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#include <wx/listctrl.h>
+
+#include <spdlog/spdlog.h>
+
+namespace tks
+{
+namespace Core
+{
+class Environment;
+} // namespace Core
+
+namespace UI::dlg
+{
+class CategoriesDialog final : public wxDialog
+{
+public:
+    CategoriesDialog() = delete;
+    CategoriesDialog(const CategoriesDialog&&) = delete;
+    CategoriesDialog(wxWindow* parent,
+        std::shared_ptr<Core::Environment> env,
+        std::shared_ptr<spdlog::logger> logger,
+        bool isEdit = false,
+        const wxString& name = "categoriesdlg");
+    virtual ~CategoriesDialog() = default;
+
+    CategoriesDialog& operator=(const CategoriesDialog&&) = delete;
+
+private:
+    void Create();
+
+    void CreateControls();
+    void FillControls();
+    void ConfigureEventBindings();
+
+    // void AppendListControlEntry(Model::CategoryModel category);
+    // void UpdateListControlEntry(Model::CategoryModel category);
+
+    void OnClear(wxCommandEvent& event);
+    void OnAdd(wxCommandEvent& event);
+    void OnEdit(wxCommandEvent& event);
+    void OnRemove(wxCommandEvent& event);
+    void OnRemoveAll(wxCommandEvent& event);
+    void OnOK(wxCommandEvent& event);
+    void OnCancel(wxCommandEvent& event);
+
+    void OnItemChecked(wxListEvent& event);
+    void OnItemUnchecked(wxListEvent& event);
+    void OnItemRightClick(wxListEvent& event);
+
+    void ResetControlValues();
+
+    bool TransferDataAndValidate();
+};
+} // namespace UI::dlg
+} // namespace tks
