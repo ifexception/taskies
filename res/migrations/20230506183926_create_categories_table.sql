eb61b6ca7b08ae63ff56f5df0c7011370db49450
Szymon Welgus <szymonwelgus@gmail.com>
2023-05-06 18:47:19 +0200

    feat: add migration for create categories table

---
+++ res/migrations/20230506183926_create_categories_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS categories
+(
+    category_id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL UNIQUE,
+    color INTEGER NOT NULL,
+    billable INTEGER NOT NULL DEFAULT (0),
+    description TEXT NULL,
+    date_created INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    date_modified INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    is_active INTEGER NOT NULL DEFAULT (1)
+);
