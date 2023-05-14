6c9a896c1e5ef53523d7fb59d45b2a6384f2bff0
Szymon Welgus <szymonwelgus@gmail.com>
2023-05-14 17:07:05 +0200

    feat: add software component list view and footer

--- src/ui/dlg/aboutdlg.cpp
+++ src/ui/dlg/aboutdlg.cpp
@@ -20,9 +20,14 @@
 #include "aboutdlg.h"
 
 #include <wx/collpane.h>
+#include <wx/hyperlink.h>
+#include <wx/listctrl.h>
 #include <wx/statline.h>
 
+#include <fmt/format.h>
+
 #include "../../common/common.h"
+#include "../../common/version.h"
 
 namespace tks::UI::dlg
 {
@@ -53,17 +58,23 @@ void AboutDialog::CreateControls()
     wxIcon icon = iconBundle.GetIcon(wxSize(128, 128));
     auto staticBmp = new wxStaticBitmap(this, wxID_ANY, icon);
     sizer->Add(staticBmp, wxSizerFlags().Border(wxLEFT | wxRIGHT, FromDIP(8)).Center());
+
+    /* Taskies version */
+    auto version = fmt::format("Taskies v{0}.{1}.{2}", TASKIES_MAJOR, TASKIES_MINOR, TASKIES_PATCH);
+    auto versionLabel = new wxStaticText(this, wxID_ANY, version);
+    sizer->Add(versionLabel, wxSizerFlags().Border(wxALL, FromDIP(5)).Center());
+
     sizer->AddSpacer(FromDIP(5));
 
     /* Description */
     auto descriptionSizer = new wxBoxSizer(wxHORIZONTAL);
     sizer->Add(descriptionSizer, wxSizerFlags().Expand());
-    auto description = "Taskies is a time tracking productivity tool built with wxWidgets, SQLite, toml11, date, fmt, "
-                       "nlohmann_json and spdlog";
-    auto descriptionCtrl =
-        new wxTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE | wxTE_READONLY);
+    auto description = "Taskies is a time tracking productivity tool built with date, fmt, nlohmann_json, spdlog, "
+                       "SQLite, and wxWidgets";
+    auto descriptionCtrl = new wxTextCtrl(
+        this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(340, -1), wxTE_MULTILINE | wxTE_READONLY);
     descriptionCtrl->AppendText(description);
-    descriptionSizer->Add(descriptionCtrl, wxSizerFlags(1).Border(wxALL, FromDIP(5)));
+    descriptionSizer->Add(descriptionCtrl, wxSizerFlags().Border(wxALL, FromDIP(5)).Expand().Proportion(1));
 
     /* License collaspible pane */
     auto licenseCollPane = new wxCollapsiblePane(this, wxID_ANY, "License");
@@ -80,15 +91,76 @@ void AboutDialog::CreateControls()
         wxDefaultSize,
         wxTE_MULTILINE | wxTE_READONLY);
     licenseTextCtrl->AppendText(Common::GetLicense());
-    licenseCollPaneWindowSizer->Add(licenseTextCtrl, wxSizerFlags(1).Border(wxALL, FromDIP(5)).Expand());
+    licenseCollPaneWindowSizer->Add(licenseTextCtrl, wxSizerFlags().Border(wxALL, FromDIP(5)).Expand().Proportion(1));
 
     licenseCollPaneWindow->SetSizer(licenseCollPaneWindowSizer);
     licenseCollPaneWindowSizer->SetSizeHints(licenseCollPaneWindow);
-    sizer->Add(licenseCollPane, wxSizerFlags(1).Expand());
+    sizer->Add(licenseCollPane, wxSizerFlags().Expand());
+
+    /* Software collaspible pane */
+    auto softwareCollPane = new wxCollapsiblePane(this, wxID_ANY, "Software");
+    auto* softwareCollPaneWindow = softwareCollPane->GetPane();
+
+    /* Software collaspible pane sizer */
+    auto softwareCollPaneWindowSizer = new wxBoxSizer(wxVERTICAL);
+
+    /* Software list view */
+    auto listView = new wxListView(softwareCollPaneWindow, wxID_ANY);
+    listView->AppendColumn("Component", wxLIST_FORMAT_LEFT, FromDIP(200));
+    listView->AppendColumn("Version", wxLIST_FORMAT_LEFT, FromDIP(80));
+
+    int listIndex = 0;
+    int columnIndex = 0;
+
+    listIndex = listView->InsertItem(columnIndex++, "date");
+    listView->SetItem(listIndex, columnIndex++, "3.0.1#2");
+    columnIndex = 0;
+
+    listIndex = listView->InsertItem(columnIndex++, "fmt");
+    listView->SetItem(listIndex, columnIndex++, "9.1.0#1");
+    columnIndex = 0;
+
+    listIndex = listView->InsertItem(columnIndex++, "nlohmann_json");
+    listView->SetItem(listIndex, columnIndex++, "3.11.2");
+    columnIndex = 0;
+
+    listIndex = listView->InsertItem(columnIndex++, "spdlog");
+    listView->SetItem(listIndex, columnIndex++, "1.11.0");
+    columnIndex = 0;
+
+    listIndex = listView->InsertItem(columnIndex++, "SQLite");
+    listView->SetItem(listIndex, columnIndex++, "3.40.1#3");
+    columnIndex = 0;
+
+    listIndex = listView->InsertItem(columnIndex++, "toml11");
+    listView->SetItem(listIndex, columnIndex++, "3.7.1");
+    columnIndex = 0;
+
+    listIndex = listView->InsertItem(columnIndex++, "wxWidgets");
+    listView->SetItem(listIndex, columnIndex++, "3.2.2.1#2");
+
+    softwareCollPaneWindowSizer->Add(listView, wxSizerFlags().Border(wxALL, 5).Expand().Proportion(1));
+
+    softwareCollPaneWindow->SetSizer(softwareCollPaneWindowSizer);
+    softwareCollPaneWindowSizer->SetSizeHints(softwareCollPaneWindow);
+    sizer->Add(softwareCollPane, wxSizerFlags(1).Expand());
 
     /* Footer */
     auto footerLine = new wxStaticLine(this, wxID_ANY);
-    sizer->Add(footerLine, wxSizerFlags().Border(wxALL, FromDIP(4)).Expand());
+    sizer->Add(footerLine, wxSizerFlags().Border(wxALL, FromDIP(2)).Expand());
+
+    /* Footer sizer */
+    auto footerSizer = new wxBoxSizer(wxHORIZONTAL);
+    sizer->Add(footerSizer, wxSizerFlags().Expand());
+
+    /* Copyright text */
+    auto copyrightText = new wxStaticText(this, wxID_ANY, "(C) 2023");
+    footerSizer->Add(copyrightText, wxSizerFlags().Border(wxALL, FromDIP(4)));
+    footerSizer->AddStretchSpacer();
+
+    /* Link */
+    auto link = new wxHyperlinkCtrl(this, wxID_ANY, "https://taskies.org", "https://taskies.org");
+    footerSizer->Add(link, wxSizerFlags().Border(wxALL, FromDIP(4)));
 
     SetSizerAndFit(sizer);
 }
