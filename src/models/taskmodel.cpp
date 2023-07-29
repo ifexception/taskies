943219ff4ee2e01feb145e388af1ef3b71670621
Szymon Welgus <szymonwelgus@gmail.com>
2023-07-29 20:49:17 +0200

    feat: implement task model

---
+++ src/models/taskmodel.cpp
@@ -0,0 +1,54 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2023 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#include "taskmodel.h"
+
+#include <date/date.h>
+
+namespace tks::Model
+{
+TaskModel::TaskModel()
+    : TaskId(-1)
+    , Billable(false)
+    , UniqueIdentifier()
+    , DurationHours(-1)
+    , DurationMinutes(-1)
+    , Description()
+    , DateCreated(0)
+    , DateModified(0)
+    , IsActive(false)
+    , ProjectId(-1)
+    , CategoryId(-1)
+    , WorkdayId(-1)
+{}
+
+const std::string TaskModel::GetDateCreatedString() const
+{
+    date::sys_seconds dateTime{ std::chrono::seconds{ DateCreated } };
+    std::string dateString = date::format("%Y-%m-%d %I:%M:%S %p", dateTime);
+    return dateString;
+}
+
+const std::string TaskModel::GetDateModifiedString() const
+{
+    date::sys_seconds dateTime{ std::chrono::seconds{ DateModified } };
+    std::string dateString = date::format("%Y-%m-%d %I:%M:%S %p", dateTime);
+    return dateString;
+}
+} // namespace tks::Model
