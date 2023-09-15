c5f5b7952e59b91d63b2ab26b7db3c92e12da69e
Szymon Welgus <szymonwelgus@gmail.com>
2023-09-15 22:15:05 +0200

    feat: add repo layer
    add task repo layer to fetch task plus linked project and category names, and add new model to store data

---
+++ src/repository/taskrepository.cpp
@@ -0,0 +1,185 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2023 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#include "taskrepository.h"
+
+#include "../common/constants.h"
+
+#include "../utils/utils.h"
+
+namespace tks::repos
+{
+TaskRepository::TaskRepository(const std::shared_ptr<spdlog::logger> logger, const std::string& databaseFilePath)
+    : pLogger(logger)
+    , pDb(nullptr)
+{
+    pLogger->info(LogMessage::InfoOpenDatabaseConnection, "TaskRepository", databaseFilePath);
+
+    int rc = sqlite3_open(databaseFilePath.c_str(), &pDb);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::OpenDatabaseTemplate, "TaskRepository", databaseFilePath, rc, std::string(err));
+    }
+
+    rc = sqlite3_exec(pDb, Utils::sqlite::pragmas::ForeignKeys, nullptr, nullptr, nullptr);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::ExecQueryTemplate, "TaskRepository", Utils::sqlite::pragmas::ForeignKeys, rc, err);
+        return;
+    }
+
+    rc = sqlite3_exec(pDb, Utils::sqlite::pragmas::JournalMode, nullptr, nullptr, nullptr);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::ExecQueryTemplate, "TaskRepository", Utils::sqlite::pragmas::JournalMode, rc, err);
+        return;
+    }
+
+    rc = sqlite3_exec(pDb, Utils::sqlite::pragmas::Synchronous, nullptr, nullptr, nullptr);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::ExecQueryTemplate, "TaskRepository", Utils::sqlite::pragmas::Synchronous, rc, err);
+        return;
+    }
+
+    rc = sqlite3_exec(pDb, Utils::sqlite::pragmas::TempStore, nullptr, nullptr, nullptr);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::ExecQueryTemplate, "TaskRepository", Utils::sqlite::pragmas::TempStore, rc, err);
+        return;
+    }
+
+    rc = sqlite3_exec(pDb, Utils::sqlite::pragmas::MmapSize, nullptr, nullptr, nullptr);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::ExecQueryTemplate, "TaskRepository", Utils::sqlite::pragmas::MmapSize, rc, err);
+        return;
+    }
+}
+
+TaskRepository::~TaskRepository()
+{
+    sqlite3_close(pDb);
+    pLogger->info(LogMessage::InfoCloseDatabaseConnection, "TaskRepository");
+}
+
+int TaskRepository::GetByDateRange(std::vector<std::string> dates, std::vector<TaskRepositoryModel> models)
+{
+    for (const auto& date : dates) {
+        pLogger->info(LogMessage::InfoBeginGetByIdEntity, "TaskRepository", "task", date);
+
+        sqlite3_stmt* stmt = nullptr;
+        TaskRepositoryModel model;
+
+        int rc = sqlite3_prepare_v2(pDb,
+            TaskRepository::getByDateRange.c_str(),
+            static_cast<int>(TaskRepository::getByDateRange.size()),
+            &stmt,
+            nullptr);
+        if (rc != SQLITE_OK) {
+            const char* err = sqlite3_errmsg(pDb);
+            pLogger->error(
+                LogMessage::PrepareStatementTemplate, "TaskRepository", TaskRepository::getByDateRange, rc, err);
+            sqlite3_finalize(stmt);
+            return -1;
+        }
+
+        rc = sqlite3_bind_text(stmt, 1, date.c_str(), static_cast<int>(date.size()), SQLITE_TRANSIENT);
+        if (rc != SQLITE_OK) {
+            const char* err = sqlite3_errmsg(pDb);
+            pLogger->error(LogMessage::BindParameterTemplate, "TaskRepository", "date", 1, rc, err);
+            sqlite3_finalize(stmt);
+            return -1;
+        }
+
+        rc = sqlite3_step(stmt);
+        if (rc != SQLITE_ROW) {
+            const char* err = sqlite3_errmsg(pDb);
+            pLogger->error(LogMessage::ExecStepTemplate, "TaskRepository", TaskRepository::getByDateRange, rc, err);
+            sqlite3_finalize(stmt);
+            return -1;
+        }
+
+        int columnIndex = 0;
+        model.TaskId = sqlite3_column_int64(stmt, columnIndex++);
+        model.Billable = !!sqlite3_column_int(stmt, columnIndex++);
+        if (sqlite3_column_type(stmt, columnIndex) == SQLITE_NULL) {
+            model.UniqueIdentifier = std::nullopt;
+        } else {
+            const unsigned char* res = sqlite3_column_text(stmt, columnIndex);
+            model.UniqueIdentifier = std::make_optional(
+                std::string(reinterpret_cast<const char*>(res), sqlite3_column_bytes(stmt, columnIndex)));
+        }
+        columnIndex++;
+
+        model.Hours = sqlite3_column_int(stmt, columnIndex++);
+        model.Minutes = sqlite3_column_int(stmt, columnIndex++);
+        const unsigned char* res = sqlite3_column_text(stmt, columnIndex);
+        model.Description = std::string(reinterpret_cast<const char*>(res), sqlite3_column_bytes(stmt, columnIndex++));
+        model.DateCreated = sqlite3_column_int(stmt, columnIndex++);
+        model.DateModified = sqlite3_column_int(stmt, columnIndex++);
+        model.IsActive = !!sqlite3_column_int(stmt, columnIndex++);
+        model.ProjectId = sqlite3_column_int64(stmt, columnIndex++);
+        model.CategoryId = sqlite3_column_int64(stmt, columnIndex++);
+        model.WorkdayId = sqlite3_column_int64(stmt, columnIndex++);
+        res = sqlite3_column_text(stmt, columnIndex);
+        model.ProjectName = std::string(reinterpret_cast<const char*>(res), sqlite3_column_bytes(stmt, columnIndex++));
+        res = sqlite3_column_text(stmt, columnIndex);
+        model.CategoryName = std::string(reinterpret_cast<const char*>(res), sqlite3_column_bytes(stmt, columnIndex++));
+
+        rc = sqlite3_step(stmt);
+        if (rc != SQLITE_DONE) {
+            const char* err = sqlite3_errmsg(pDb);
+            pLogger->warn(LogMessage::ExecStepMoreResultsThanExpectedTemplate, "TaskRepository", rc, err);
+            sqlite3_finalize(stmt);
+            return -1;
+        }
+
+        models.push_back(model);
+        sqlite3_finalize(stmt);
+        pLogger->info(LogMessage::InfoEndGetByIdEntity, "TaskRepository", date);
+    }
+
+    return 0;
+}
+
+const std::string TaskRepository::getByDateRange = "SELECT "
+                                                   "tasks.task_id, "
+                                                   "tasks.billable, "
+                                                   "tasks.unique_identifier, "
+                                                   "tasks.hours, "
+                                                   "tasks.minutes, "
+                                                   "tasks.description, "
+                                                   "tasks.date_created, "
+                                                   "tasks.date_modified, "
+                                                   "tasks.is_active, "
+                                                   "tasks.project_id, "
+                                                   "tasks.category_id, "
+                                                   "tasks.workday_id, "
+                                                   "projects.display_name,"
+                                                   "categories.name "
+                                                   "FROM tasks "
+                                                   "INNER JOIN workdays "
+                                                   "ON tasks.workday_id = workdays.workday_id "
+                                                   "INNER JOIN projects "
+                                                   "ON tasks.project_id = projects.project_id "
+                                                   "INNER JOIN categories "
+                                                   "ON tasks.category_id = categories.category_id "
+                                                   "WHERE workdays.date = ?;";
+} // namespace tks::repos
---
+++ src/repository/taskrepository.h
@@ -0,0 +1,53 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2023 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <spdlog/logger.h>
+
+#include <sqlite3.h>
+
+#include "taskrepositorymodel.h"
+
+namespace tks::repos
+{
+class TaskRepository final
+{
+public:
+    TaskRepository() = delete;
+    TaskRepository(const TaskRepository&) = delete;
+    TaskRepository(const std::shared_ptr<spdlog::logger> logger, const std::string& databaseFilePath);
+    ~TaskRepository();
+
+    TaskRepository& operator=(const TaskRepository&) = delete;
+
+    int GetByDateRange(std::vector<std::string> dates, /*out*/ std::vector<TaskRepositoryModel> models);
+
+private:
+    std::shared_ptr<spdlog::logger> pLogger;
+    sqlite3* pDb;
+
+    static const std::string getByDateRange;
+};
+} // namespace tks::repos
