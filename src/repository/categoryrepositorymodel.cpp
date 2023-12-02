46e863595e5f0c7be98f2b04d3418188eec56305
Szymon Welgus <szymonwelgus@gmail.com>
2023-12-02 14:26:05 +0200

    feat: add categoryrepositorymodel

---
+++ src/repository/categoryrepositorymodel.cpp
@@ -0,0 +1,61 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2023 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#include "categoryrepositorymodel.h"
+
+#include <date/date.h>
+#include <fmt/format.h>
+
+#include "../utils/utils.h"
+
+namespace tks::repos
+{
+CategoryRepositoryModel::CategoryRepositoryModel()
+    : CategoryId(-1)
+    , Name()
+    , Color(0)
+    , Billable(false)
+    , Description()
+    , DateCreated(0)
+    , DateModified(0)
+    , IsActive(false)
+    , ProjectId()
+    , ProjectDisplayName()
+{
+}
+
+std::string CategoryRepositoryModel::GetFormattedName()
+{
+    return fmt::format("({0}) - {1}", ProjectDisplayName, Name);
+}
+
+const std::string CategoryRepositoryModel::GetDateCreatedString() const
+{
+    date::sys_seconds dateTime{ std::chrono::seconds{ DateCreated } };
+    std::string dateString = date::format("%Y-%m-%d %I:%M:%S %p", dateTime);
+    return dateString;
+}
+
+const std::string CategoryRepositoryModel::GetDateModifiedString() const
+{
+    date::sys_seconds dateTime{ std::chrono::seconds{ DateModified } };
+    std::string dateString = date::format("%Y-%m-%d %I:%M:%S %p", dateTime);
+    return dateString;
+}
+} // namespace tks::repos
---
+++ src/repository/categoryrepositorymodel.h
@@ -0,0 +1,48 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2023 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <string>
+
+namespace tks::repos
+{
+struct CategoryRepositoryModel {
+    CategoryRepositoryModel();
+
+    std::int64_t CategoryId;
+    std::string Name;
+    unsigned int Color;
+    bool Billable;
+    std::optional<std::string> Description;
+    std::int32_t DateCreated;
+    std::int32_t DateModified;
+    bool IsActive;
+    std::optional<std::int64_t> ProjectId;
+
+    std::string ProjectDisplayName;
+
+    std::string GetFormattedName();
+
+    const std::string GetDateCreatedString() const;
+    const std::string GetDateModifiedString() const;
+};
+}
