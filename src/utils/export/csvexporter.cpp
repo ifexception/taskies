855cb9dbd6c0634adfd38bf36ec16e8e2bcbe9f8
Szymon Welgus <szymonwelgus@gmail.com>
2024-06-15 15:40:40 +0200

    feat: initial commit of csvexporter class

---
+++ src/utils/export/csvexporter.cpp
@@ -0,0 +1,28 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2024 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#include "csvexporter.h"
+
+namespace tks::Utils
+{
+CsvExporter::CsvExporter(std::shared_ptr<spdlog::logger> logger)
+    : pLogger(logger)
+{
+}
+} // namespace tks::Utils
