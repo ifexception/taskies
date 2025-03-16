22d093079722129d0b5c3fd503b3ba4b3374d0c4
Szymon Welgus <szymonwelgus@gmail.com>
2025-03-16 15:36:34 +0200

    feat: initial commit of AttributeGroupsPersistence

---
+++ src/persistence/attributegroupspersistence.h
@@ -0,0 +1,44 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2025 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <spdlog/logger.h>
+
+#include <sqlite3.h>
+
+namespace tks::Persistence
+{
+class AttributeGroupsPersistence final
+{
+public:
+    AttributeGroupsPersistence(std::shared_ptr<spdlog::logger> logger,
+        const std::string& databaseFilePath);
+    ~AttributeGroupsPersistence();
+
+private:
+    std::shared_ptr<spdlog::logger> pLogger;
+    sqlite3* pDb;
+};
+} // namespace tks::Persistence
