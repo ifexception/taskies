462f4664cbf39796a9ac23ff72c4b046331bcb57
Szymon Welgus <szymonwelgus@gmail.com>
2025-03-17 20:16:35 +0200

    feat: implement filter method for attribute groups

--- src/persistence/attributegroupspersistence.cpp
+++ src/persistence/attributegroupspersistence.cpp
@@ -114,6 +114,119 @@ AttributeGroupsPersistence::~AttributeGroupsPersistence()
 int AttributeGroupsPersistence::Filter(const std::string& searchTerm,
     std::vector<Model::AttributeGroupModel>& attributeGroupModels)
 {
+    SPDLOG_LOGGER_TRACE(pLogger,
+        LogMessage::InfoBeginFilterEntities,
+        "AttributeGroupsPersistence",
+        "attribute groups",
+        searchTerm);
+
+    auto formatedSearchTerm = Utils::sqlite::FormatSearchTerm(searchTerm);
+
+    sqlite3_stmt* stmt = nullptr;
+    int rc = sqlite3_prepare_v2(pDb,
+        AttributeGroupsPersistence::filter.c_str(),
+        static_cast<int>(AttributeGroupsPersistence::filter.size()),
+        &stmt,
+        nullptr);
+
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::PrepareStatementTemplate,
+            "AttributeGroupsPersistence",
+            AttributeGroupsPersistence::filter,
+            rc,
+            err);
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+
+    // name
+    rc = sqlite3_bind_text(stmt,
+        1,
+        formatedSearchTerm.c_str(),
+        static_cast<int>(formatedSearchTerm.size()),
+        SQLITE_TRANSIENT);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(
+            LogMessage::BindParameterTemplate, "AttributeGroupsPersistence", "name", 1, rc, err);
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+
+    // description
+    rc = sqlite3_bind_text(stmt,
+        2,
+        formatedSearchTerm.c_str(),
+        static_cast<int>(formatedSearchTerm.size()),
+        SQLITE_TRANSIENT);
+    if (rc != SQLITE_OK) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::BindParameterTemplate,
+            "AttributeGroupsPersistence",
+            "description",
+            2,
+            rc,
+            err);
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+
+    bool done = false;
+    while (!done) {
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_ROW: {
+            rc = SQLITE_ROW;
+            Model::AttributeGroupModel model;
+
+            int columnIndex = 0;
+            model.AttributeGroupId = sqlite3_column_int64(stmt, columnIndex++);
+            const unsigned char* res = sqlite3_column_text(stmt, columnIndex);
+            model.Name = std::string(
+                reinterpret_cast<const char*>(res), sqlite3_column_bytes(stmt, columnIndex++));
+            if (sqlite3_column_type(stmt, columnIndex) == SQLITE_NULL) {
+                model.Description = std::nullopt;
+            } else {
+                const unsigned char* res = sqlite3_column_text(stmt, columnIndex);
+                model.Description = std::string(
+                    reinterpret_cast<const char*>(res), sqlite3_column_bytes(stmt, columnIndex));
+            }
+            columnIndex++;
+            model.DateCreated = sqlite3_column_int(stmt, columnIndex++);
+            model.DateModified = sqlite3_column_int(stmt, columnIndex++);
+            model.IsActive = !!sqlite3_column_int(stmt, columnIndex++);
+
+            attributeGroupModels.push_back(model);
+            break;
+        }
+        case SQLITE_DONE:
+            rc = SQLITE_DONE;
+            done = true;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (rc != SQLITE_DONE) {
+        const char* err = sqlite3_errmsg(pDb);
+        pLogger->error(LogMessage::ExecStepTemplate,
+            "AttributeGroupsPersistence",
+            AttributeGroupsPersistence::filter,
+            rc,
+            err);
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+
+    sqlite3_finalize(stmt);
+
+    SPDLOG_LOGGER_TRACE(pLogger,
+        LogMessage::InfoEndFilterEntities,
+        "AttributeGroupsPersistence",
+        attributeGroupModels.size(),
+        searchTerm);
+
     return 0;
 }
 
@@ -265,7 +378,7 @@ std::int64_t AttributeGroupsPersistence::Create(
         pLogger->error(LogMessage::BindParameterTemplate,
             "AttributeGroupsPersistence",
             "description",
-            3,
+            2,
             rc,
             err);
         sqlite3_finalize(stmt);
