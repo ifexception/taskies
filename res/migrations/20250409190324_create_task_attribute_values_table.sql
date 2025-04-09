370f61815c62bcbd62ca548deb85eb2ab63b7537
Szymon Welgus <szymonwelgus@gmail.com>
2025-04-09 19:40:05 +0200

    fix: attribute values should allow to be NULL

--- res/migrations/20250409190324_create_task_attribute_values_table.sql
+++ res/migrations/20250409190324_create_task_attribute_values_table.sql
@@ -2,9 +2,9 @@ CREATE TABLE IF NOT EXISTS task_attribute_values
 (
     task_attribute_value_id INTEGER PRIMARY KEY NOT NULL,
 
-    text_value TEXT NOT NULL,
-    boolean_value INTEGER NOT NULL,
-    numeric_value INTEGER NOT NULL,
+    text_value TEXT NULL,
+    boolean_value INTEGER NULL,
+    numeric_value INTEGER NULL,
 
     date_created INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
     date_modified INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
