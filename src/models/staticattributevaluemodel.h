7a23c323fb6164cb82ec3c945a4e4d5aad7b3dfd
Szymon Welgus <szymonwelgus@gmail.com>
2025-04-27 16:58:56 +0200

    feat: add model struct for static attribute values

---
+++ src/models/staticattributevaluemodel.h
@@ -0,0 +1,48 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2025 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <string>
+
+namespace tks::Model
+{
+struct StaticAttributeValueModel {
+    StaticAttributeValueModel();
+
+    std::int64_t StaticAttributeValueId;
+
+    std::optional<std::string> TextValue;
+    std::optional<bool> BooleanValue;
+    std::optional<int> NumericValue;
+    bool IsStatic;
+
+    std::uint32_t DateCreated;
+    std::uint32_t DateModified;
+    bool IsActive;
+
+    std::int64_t AttributeGroupId;
+    std::int64_t AttributeId;
+
+    const std::string GetDateCreatedString() const;
+    const std::string GetDateModifiedString() const;
+};
+} // namespace tks::Model
