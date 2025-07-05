28d86c8beb6ceba853f0a2bacf497eb15da2cae1
Szymon Welgus <szymonwelgus@gmail.com>
2025-07-05 16:38:47 +0200

    fix: rework model structs for main data and attributes

--- src/services/export/projectionkeyvaluepairmodel.cpp
+++ src/services/export/projectionkeyvaluepairmodel.cpp
@@ -1,14 +1,1 @@
 #include "projectionkeyvaluepairmodel.h"
-
-ProjectionKeyValuePairModel::ProjectionKeyValuePairModel(const std::string& key,
-    const std::string& value)
-    : Key(key)
-    , Value(value)
-{
-}
-
-ProjectionListModel::ProjectionListModel()
-    : TaskId(-1)
-    , ProjectionKeyValuePairModels()
-{
-}
--- src/services/export/projectionkeyvaluepairmodel.h
+++ src/services/export/projectionkeyvaluepairmodel.h
@@ -4,19 +4,69 @@
 #include <string>
 #include <vector>
 
-struct ProjectionKeyValuePairModel {
-    ProjectionKeyValuePairModel() = delete;
-    ProjectionKeyValuePairModel(const std::string& key, const std::string& value);
-    ~ProjectionKeyValuePairModel() = default;
+struct ColumnValueModel {
+    ColumnValueModel()
+        : Column()
+        , Value()
+    {
+    }
+    ColumnValueModel(std::string column, std::string value)
+        : Column(column)
+        , Value(value)
+    {
+    }
 
-    std::string Key;
+    /*
+     * The column field is not used anywhere except for debugging purposes to easier pair (and see)
+     * the value
+     */
+    std::string Column;
     std::string Value;
 };
 
-struct ProjectionListModel {
-    ProjectionListModel();
-    ~ProjectionListModel() = default;
+struct ValuesModel {
+    ValuesModel()
+        : ColumnValueModels()
+    {
+    }
+
+    std::vector<ColumnValueModel> ColumnValueModels;
+};
+
+// -- ATTRIBUTES
+
+constexpr int ATTRIBUTE_PROP_INDEX_TASKID = 0;
+constexpr int ATTRIBUTE_PROP_INDEX_NAME = 1;
+constexpr int ATTRIBUTE_PROP_INDEX_VALUE = 2;
+
+struct AttributeHeaderValueModel {
+    AttributeHeaderValueModel()
+        : Header()
+        , Value()
+    {
+    }
+    AttributeHeaderValueModel(std::string& header, std::string& value)
+        : Header(header)
+        , Value(value)
+    {
+    }
+
+    std::string Header;
+    std::string Value;
+};
+
+struct AttributeValueModel {
+    AttributeValueModel()
+        : TaskId(-1)
+        , HeaderValueModels()
+    {
+    }
+    AttributeValueModel(std::int64_t taskId)
+        : TaskId(taskId)
+        , HeaderValueModels()
+    {
+    }
 
     std::int64_t TaskId;
-    std::vector<ProjectionKeyValuePairModel> ProjectionKeyValuePairModels;
+    std::vector<AttributeHeaderValueModel> HeaderValueModels;
 };
