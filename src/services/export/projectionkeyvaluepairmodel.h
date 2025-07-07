ed8528d3ea8521308c7a1521cb3bf91a0a974447
Szymon Welgus <szymonwelgus@gmail.com>
2025-07-07 19:06:13 +0200

    refactor: remove unneeded struct models

--- src/services/export/projectionkeyvaluepairmodel.h
+++ src/services/export/projectionkeyvaluepairmodel.h
@@ -5,35 +5,6 @@
 #include <unordered_map>
 #include <vector>
 
-struct ColumnValueModel {
-    ColumnValueModel()
-        : Column()
-        , Value()
-    {
-    }
-    ColumnValueModel(std::string column, std::string value)
-        : Column(column)
-        , Value(value)
-    {
-    }
-
-    /*
-     * The column field is not used anywhere except for debugging purposes to easier pair (and see)
-     * the value
-     */
-    std::string Column;
-    std::string Value;
-};
-
-struct ValuesModel {
-    ValuesModel()
-        : ColumnValueModels()
-    {
-    }
-
-    std::vector<ColumnValueModel> ColumnValueModels;
-};
-
 // -- ATTRIBUTES
 
 constexpr int ATTRIBUTE_PROP_INDEX_TASKID = 0;
