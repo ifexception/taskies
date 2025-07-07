f5000be2e2137248518f7854a8eb54724eab7446
Szymon Welgus <szymonwelgus@gmail.com>
2025-07-07 20:06:49 +0200

    refactor: header* structs into their own files

--- src/services/export/projectionkeyvaluepairmodel.h
+++ src/services/export/projectionkeyvaluepairmodel.h
@@ -5,35 +5,6 @@
 #include <unordered_map>
 #include <vector>
 
-constexpr int ATTRIBUTE_PROP_INDEX_TASKID = 0;
-constexpr int ATTRIBUTE_PROP_INDEX_NAME = 1;
-constexpr int ATTRIBUTE_PROP_INDEX_VALUE = 2;
-
-struct HeaderValuePair {
-    HeaderValuePair()
-        : Header("")
-        , Value("")
-    {
-    }
-    HeaderValuePair(std::string header, std::string value)
-        : Header(header)
-        , Value(value)
-    {
-    }
-
-    std::string Header;
-    std::string Value;
-};
-
-struct HeaderValueRow {
-    HeaderValueRow()
-        : HeaderValuePairs()
-    {
-    }
-
-    std::vector<HeaderValuePair> HeaderValuePairs;
-};
-
 struct Row {
     std::vector<std::string> Values;
 };
