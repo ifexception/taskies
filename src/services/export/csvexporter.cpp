f3ddca8b7b8fc1338f70fc5a5280e39d580a1348
Szymon Welgus <szymonwelgus@gmail.com>
2025-07-10 20:30:14 +0200

    feat: document the export procedure

--- src/services/export/csvexporter.cpp
+++ src/services/export/csvexporter.cpp
@@ -73,47 +73,76 @@ bool CsvExporter::GenerateExport(const std::vector<Projection>& projections,
     const std::string& toDate,
     std::string& exportedDataPreview)
 {
-    std::unordered_map<std::int64_t, Row> rows;
+    /* initialize variables */
     int rc = -1;
+    ExportsService exportsService(mDatabaseFilePath, pLogger);
+    std::unordered_map<std::int64_t, Row> rows;
 
+    /*
+     * get the headers from the projections built out from selected items from the list view
+     * use the `UserColumn` as this is what the user renamed a potential header to
+     * if a user did not rename a header, then it defaults to the "display" name
+     */
     std::vector<std::string> headers = GetHeadersFromProjections(projections);
     if (headers.empty()) {
         return false;
     }
 
+    /*
+     * build the dynamic query factoring the projections built out from the selected items
+     * from the list view, including the computed join projection, plus the from and to date range
+     */
     const auto& sql = pQueryBuilder->BuildQuery(projections, joinProjections, fromDate, toDate);
 
-    ExportsService exportsService(mDatabaseFilePath, pLogger);
-
+    /*
+     * get the actual values (in the order the projections were built from the user selected items)
+     * from the database
+     * `headers.size()` indicates the number of values per row we need to retrieve
+     * the `taskId` is the unique key for which each row that is fetched
+     * the `taskId` is crucial for linking attributes
+     * if the `includeAttributes` option is false, then it is not used
+     * each row contains a std::vector of std::string's
+     * each std::string contains the corresponding value relating to the headers position
+     * see the function `FilterExportCsvData` for more detail
+     */
     rc = exportsService.FilterExportCsvData(sql, headers.size(), rows);
     if (rc != 0) {
         return false;
     }
 
+    /* `SData` is our main struct to store the headers and rows */
     SData exportData;
 
-    // step 1: build out computed header list
+    /* insert the headers we got from the projections into the `Headers` field of `SData` */
     for (auto& header : headers) {
         exportData.Headers.push_back(header);
     }
 
-    // step 2: attach "main" rows to 'Data' struct
+    /* set the task row values into the `Rows` field of `SData` */
     exportData.Rows = rows;
 
     if (mOptions.IncludeAttributes) {
+        /* see `GenerateAttributes` for more detail */
         bool attributeAddedSuccessfully = GenerateAttributes(fromDate, toDate, exportData);
         if (!attributeAddedSuccessfully) {
             return false;
         }
     }
 
+    /*
+     * initialize the csv mapped options object to map delimiter and text qualifier options
+     * to a char value to be used in the processor
+     */
     CsvMappedOptions mappedOptions(mOptions);
 
+    /* initialize the export value processor with the csv options and their mapped options */
     CsvExportProcessor exportProcessor(mOptions, mappedOptions);
 
     std::stringstream exportedData;
 
+    /* check if the user opted out to include headers */
     if (!mOptions.ExcludeHeaders) {
+        /* append all headers from the `SData` `Headers` field to our stringstream object */
         for (auto i = 0; i < exportData.Headers.size(); i++) {
             exportedData << exportData.Headers[i];
             if (i < exportData.Headers.size() - 1) {
@@ -123,11 +152,21 @@ bool CsvExporter::GenerateExport(const std::vector<Projection>& projections,
         exportedData << "\n";
     }
 
+    /*
+     * loop over row from our `SData` struct `Rows` field
+     */
     for (const auto& row : exportData.Rows) {
+        /* get our row values std::vector */
         const auto& rowValues = row.second.Values;
         for (size_t i = 0; i < rowValues.size(); i++) {
+            /*
+             * process / modify each value individually through the export processor
+             * and apply the csv options where applicable and alter the value accordingly
+             */
             std::string value = rowValues[i];
             exportProcessor.ProcessData(value);
+
+            /* append the processed value to our stringstream object */
             exportedData << value;
 
             if (i < rowValues.size() - 1) {
@@ -138,10 +177,12 @@ bool CsvExporter::GenerateExport(const std::vector<Projection>& projections,
         exportedData << "\n";
     }
 
+    /* verify the data in stringstream is in a good state */
     if (!exportedData.good()) {
         return false;
     }
 
+    /* set the out string and return */
     exportedDataPreview = exportedData.str();
     return true;
 }
@@ -150,45 +191,80 @@ bool CsvExporter::GenerateAttributes(const std::string& fromDate,
     const std::string& toDate,
     SData& data)
 {
+    /* initialize variables */
     std::optional<std::int64_t> taskId;
     ExportsService exportsService(mDatabaseFilePath, pLogger);
     int rc = -1;
     std::vector<std::string> attributeNames;
+    std::unordered_map<std::int64_t, HeaderValueRow> attributeHeaderValueRows;
 
     if (bIsPreview) {
+        /* if we are previewing data, `data.Rows.size()` should be one */
         assert(data.Rows.size() == 1);
+        /*
+         * with that information, we know that the below loop will only iterate over the rows
+         * once and get the correct key / `taskId`
+         */
         for (auto& row : data.Rows) {
             taskId = std::make_optional(row.first);
         }
     }
 
+    /*
+     * fetch all the attribute names (headers) that have been used for from and to date range
+     * if we are in preview mode, only fetch attribute names for the date range and where
+     * `taskId` matches
+     * SQLite by defaults return the attribute names sorted alphabetically ascending
+     */
+    rc = exportsService.GetAttributeNames(fromDate, toDate, taskId, bIsPreview, attributeNames);
+    if (rc != 0) {
+        return false;
+    }
+
+    /* we have not found any attributes associated with the task or tasks so we can return */
+    if (attributeNames.empty()) {
+        return true;
+    }
+
+    /*
+     * generate the query to get the attributes as a "header-value pair for from and to range
+     * and if we are in preview mode, where `taskId` matches
+     */
     const std::string& attributeSql = pQueryBuilder->BuildAttributesQuery(fromDate, toDate, taskId);
 
-    std::unordered_map<std::int64_t, HeaderValueRow> attributeHeaderValueRows;
+    /*
+     * get the actual attribute names (headers) and their values and insert the pair into
+     * the std::unordered_map with (again) the `taskId` serving as the unique key
+     */
     rc = exportsService.FilterExportCsvAttributesData(attributeSql, attributeHeaderValueRows);
 
     if (rc != 0) {
         return false;
     }
 
-    rc = exportsService.GetAttributeNames(fromDate, toDate, taskId, bIsPreview, attributeNames);
-    if (rc != 0) {
-        return false;
-    }
-    if (attributeNames.empty()) {
-        return true;
-    }
-
-    // step 3: append attribute header (name) list to main struct
-    // (ordered by ASC name by SQLite)
+    /* append the attribute names (headers) to the `SData` field `Headers` (order is important) */
     for (auto& attributeName : attributeNames) {
         data.Headers.push_back(attributeName);
     }
 
-    // step 4: attach attributes to their index in attributeNames list
+    /*
+     * for each `taskId` key and corresponding row
+     */
     for (auto& [taskIdKey, row] : data.Rows) {
+        /* get the attribute header value pairs where we match with the `taskId` key */
         auto& headerValuePair = attributeHeaderValueRows[taskIdKey].HeaderValuePairs;
 
+        /*
+         * we loop over attribute names (headers)
+         * we do another loop over the header-value pairs retrieved from the database
+         * until we find a header match
+         * we then store value from the header-value pair in the `value` variable
+         * importantly, we then break out the loop so we insert the value at the correct index
+         * in the row of the `taskId` we are currently appending attributes to
+         * if there is no match, then we just insert a blank (empty string) into the
+         * row values std::vector at that index
+         * and repeat
+         */
         for (size_t i = 0; i < attributeNames.size(); i++) {
             std::string value = "";
 
