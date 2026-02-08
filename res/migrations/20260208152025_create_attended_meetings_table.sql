75b8f7288541947fd3b4fa86c80a150996d9e342
Szymon Welgus <szymonwelgus@gmail.com>
2026-02-08 16:52:26 +0200

    feat: migration for table to store attended meetings

---
+++ res/migrations/20260208152025_create_attended_meetings_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS attended_meetings
+(
+    attended_meeting_id INTEGER PRIMARY KEY NOT NULL,
+
+    entry_id TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    start TEXT NOT NULL,
+    end TEXT NOT NULL,
+    duration TEXT NOT NULL,
+    location TEXT NOT NULL
+
+    date_created INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    date_modified INTEGER NOT NULL DEFAULT (strftime('%s','now', 'localtime')),
+    is_active INTEGER NOT NULL DEFAULT (1)
+);
