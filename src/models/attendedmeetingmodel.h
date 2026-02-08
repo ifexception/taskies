2244b08de2c7c51af61b92827f3fb12de1fe74de
Szymon Welgus <szymonwelgus@gmail.com>
2026-02-08 16:53:58 +0200

    feat: add attendedmeetingmodel and base init

---
+++ src/models/attendedmeetingmodel.h
@@ -0,0 +1,47 @@
+// Productivity tool to help you track the time you spend on tasks
+// Copyright (C) 2025 Szymon Welgus
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Contact:
+//     szymonwelgus at gmail dot com
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace tks::Model
+{
+struct AttendedMeetingModel {
+    AttendedMeetingModel();
+    ~AttendedMeetingModel() = default;
+
+    std::int64_t AttendedMeetingId;
+
+    std::string EntryId;
+    std::wstring Subject;
+    std::string Start;
+    std::string End;
+    std::string Duration;
+    std::string Location;
+
+    std::uint32_t DateCreated;
+    std::uint32_t DateModified;
+    bool IsActive;
+
+    const std::string GetDateCreatedString() const;
+    const std::string GetDateModifiedString() const;
+};
+} // namespace tks::Model
