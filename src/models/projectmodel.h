8025fb76ed595df45ae4fff7994398a995264dfb
Szymon Welgus <szymonwelgus@gmail.com>
2026-09-17 18:27:58 +0200

    feat: update project model with billable bool

--- src/models/projectmodel.h
+++ src/models/projectmodel.h
@@ -31,6 +31,7 @@ struct ProjectModel {
     std::int64_t ProjectId;
     std::string Name;
     bool IsDefault;
+    bool Billable;
     std::optional<int> BillableHours;
     std::optional<std::string> Description;
     std::int32_t DateCreated;
