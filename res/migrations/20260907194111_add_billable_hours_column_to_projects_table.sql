0b9ca5120227d2185013d813b95e8fb0c4a0a5a3
Szymon Welgus <szymonwelgus@gmail.com>
2026-09-17 18:27:41 +0200

    feat: add billable column to projects table and 'if not exists' keywords

--- res/migrations/20260907194111_add_billable_hours_column_to_projects_table.sql
+++ res/migrations/20260907194111_add_billable_hours_column_to_projects_table.sql
@@ -1,1 +1,5 @@
-ALTER TABLE projects ADD COLUMN billable_hours INT;
+ALTER TABLE
+    projects ADD COLUMN IF NOT EXISTS billable INT;
+
+ALTER TABLE projects
+    ADD COLUMN IF NOT EXISTS billable_hours INT;
